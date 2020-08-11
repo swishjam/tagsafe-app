38d09ab4e3e5ce025dc7b78ae5bad64e5b077165
Collin Schneider <collinschneider@collins-mbp.home>
2020-08-11 13:27:02 -0700

    ENV redius URL

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,11 +1,9 @@
 require 'active_scheduler'
 
-Resque.redis = 'localhost:6379'
+Resque.redis = ENV['REDIS_URL'] || 'localhost:6379'
 
 Resque.logger = Logger.new(STDOUT)
-# :debug, :info, :warn, :error, :fatal, and :unknown,
 Resque.logger.level = Logger::INFO
-# Resque.logger.formatter = Resque::VerboseFormatter.new
 
 yaml_schedule    = YAML.load_file(Rails.root.join('config', 'cron-schedule.yml'))
 wrapped_schedule = ActiveScheduler::ResqueWrapper.wrap yaml_schedule
