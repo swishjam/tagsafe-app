5174832453521acb7b3c2d17fa15c0635b9c281a
Collin Schneider <collinschneider@collins-mbp.home>
2020-08-11 20:28:16 -0700

    prettier times, QM

--- db/migrate/20200811133025_create_script_subscriber_join_table.rb
+++ db/migrate/20200811133025_create_script_subscriber_join_table.rb
@@ -1,7 +1,7 @@
 class CreateScriptSubscriberJoinTable < ActiveRecord::Migration[5.2]
   def up
     create_table :notification_subscribers do |t|
-      t.integer :script_subscriber_id
+      t.integer :monitored_script_id
       t.integer :user_id
 
       t.timestamps
