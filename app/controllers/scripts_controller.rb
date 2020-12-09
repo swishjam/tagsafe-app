e3ec50a7d8139f148711997ff77b9cfdf366ae2a
Collin Schneider <collinschneider@collins-mbp.home>
2020-12-09 14:29:12 -0800

    remove tag change count

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -9,10 +9,11 @@ class ScriptsController < LoggedInController
                                             .page(params[:page] || 1).per(params[:per_page] || 9)
       @filtered_performance_audit_metric_type = PerformanceAuditMetricType.find_by(key: params[:metric_type] || 'DOMComplete')
       # need to exclude first script changes
-      @tag_change_count = @script_subscriptions.map do |ss| 
-        ss.script.script_changes
-                  .newer_than_or_equal_to(ss.first_script_change.created_at > Date.today.beginning_of_day ? ss.first_script_change.created_at : Date.today.beginning_of_day)
-      end.flatten!.count
+      @tag_change_count = 0
+      # @tag_change_count = @script_subscriptions.map do |ss| 
+      #   ss.script.script_changes
+      #             .newer_than_or_equal_to(ss.first_script_change.created_at > Date.today.beginning_of_day ? ss.first_script_change.created_at : Date.today.beginning_of_day)
+      # end.flatten!.count
     end
   end
 end
