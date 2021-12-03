21f95d833f9b5ed61e1dd3564c53b7f8bb58a6d0
Collin Schneider <collin.thomas.schneider@gmail.com>
2021-12-02 17:27:55 -0800

    adds New Relic tag and makes demo tags configurable by ENV

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < LoggedOutController
   def index
-    @include_google_analytics = true
-    @include_thirdpartytag_dotcom = true
-    @include_segment = true
+    @include_google_analytics = ENV['INCLUDE_GOOGLE_ANALYTICS_TAG_DEMO'] == 'true'
+    @include_thirdpartytag_dotcom = ENV['INCLUDE_THIRDPARTYTAG_DOTCOM_TAG_DEMO'] == 'true'
+    @include_segment = ENV['INCLUDE_SEGMENT_TAG_DEMO'] == 'true'
+    @include_new_relic = ENV['INCLUDE_NEW_RELIC_TAG_DEMO'] == 'true'
   end
 
   def learn_more
