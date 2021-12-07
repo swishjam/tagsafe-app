98db653f80d25d67ce34142f90c718d6d7cd392d
Collin Schneider <collin.thomas.schneider@gmail.com>
2021-12-07 10:04:23 -0800

    fixes resque error rendering tag_version_table_row when audit is nil

--- db/migrate_archive_12_03/20211205172810_change_tables_to_reference_page_urls.rb
+++ db/migrate_archive_12_03/20211205172810_change_tables_to_reference_page_urls.rb
@@ -1,9 +1,9 @@
 class ChangeTablesToReferencePageUrls < ActiveRecord::Migration[6.1]
   def change
-    # add_column :page_urls, :should_scan_for_tags, :boolean
-    # add_reference :urls_to_audit, :page_url, index: true
-    # add_reference :url_crawls, :page_url, index: true
-    # add_reference :tags, :found_on_page_url, index: true
+    add_column :page_urls, :should_scan_for_tags, :boolean
+    add_reference :urls_to_audit, :page_url, index: true
+    add_reference :url_crawls, :page_url, index: true
+    add_reference :tags, :found_on_page_url, index: true
 
     remove_column :urls_to_audit, :audit_url
     remove_column :urls_to_audit, :display_url
