e15cb044b193309030af8998a58bf834bd670f00
Collin Schneider <collin.thomas.schneider@gmail.com>
2022-03-15 18:23:18 -0700

    hack to fix bad migration

--- db/migrate/20220315002959_make_num_simultaneous_performance_audits_configurable.rb
+++ db/migrate/20220315002959_make_num_simultaneous_performance_audits_configurable.rb
@@ -1,6 +1,9 @@
 class MakeNumSimultaneousPerformanceAuditsConfigurable < ActiveRecord::Migration[6.1]
-  def change
-    # add_column :default_audit_configurations, :perf_audit_batch_size, :integer
-    add_column :performance_audit_configurations, :batch_size, :integer
+  def up
+    add_column :default_audit_configurations, :perf_audit_batch_size, :integer
+    # add_column :performance_audit_configurations, :batch_size, :integer
+  end
+
+  def down
   end
 end
--- db/migrate/20220315161750_add_max_failed_performance_audits_to_default_audit_configuration.rb
+++ db/migrate/20220315161750_add_max_failed_performance_audits_to_default_audit_configuration.rb
@@ -1,7 +1,7 @@
 class AddMaxFailedPerformanceAuditsToDefaultAuditConfiguration < ActiveRecord::Migration[6.1]
   def up
-    add_column :default_audit_configurations, :perf_audit_max_failures, :integer
-    add_column :performance_audit_configurations, :max_failures, :integer
+    # add_column :default_audit_configurations, :perf_audit_max_failures, :integer
+    # add_column :performance_audit_configurations, :max_failures, :integer
     # remove_column :default_audit_configurations, :minimum_num_performance_audit_sets
     # remove_column :default_audit_configurations, :maximum_num_performance_audit_sets
     # remove_column :default_audit_configurations, :fail_performance_audit_when_confidence_range_not_met
