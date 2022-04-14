e6949be5cb99a6b0bb68e7525fe74b540f301023
Collin Schneider <collin.thomas.schneider@gmail.com>
2022-04-13 23:04:36 -0700

    udpates hirefire config

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,19 +1,18 @@
 Rails.logger.info "Initializing HireFire config..."
 HireFire::Resource.configure do |config|
-  config.dyno(:default_queue) do
+  config.dyno(:worker) do
+    HireFire::Macro::Resque.queue(:critical)
+    HireFire::Macro::Resque.queue(:normal)
+    HireFire::Macro::Resque.queue(:low)
     HireFire::Macro::Resque.queue(:default)
   end
-  
-  config.dyno(:tag_checker_queue) do
-    HireFire::Macro::Resque.queue(:tag_checker_queue)
+
+  config.dyno(:lambda_results) do
+    HireFire::Macro::Resque.queue(:lambda_results)
   end
-  
-  config.dyno(:performance_audit_runner_queue) do
-    HireFire::Macro::Resque.queue(:performance_audit_runner_queue)
-  end
-  
-  config.dyno(:functional_tests_queue) do
-    HireFire::Macro::Resque.queue(:functional_tests_queue)
+
+  config.dyno(:scheduled_audit_queue) do
+    HireFire::Macro::Resque.queue(:scheduled_audit_queue)
   end
   
   config.dyno(:resque_scheduler) do
