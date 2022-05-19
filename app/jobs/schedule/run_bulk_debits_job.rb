0f2d4810c47658504d2c920bedb39674b602b976
Collin Schneider <collin.thomas.schneider@gmail.com>
2022-05-19 16:54:11 -0400

    adds NewRelic, small tweaks

--- app/jobs/schedule/run_bulk_debits_job.rb
+++ app/jobs/schedule/run_bulk_debits_job.rb
@@ -6,7 +6,7 @@ module Schedule
       Rails.logger.info "RunBulkDebitsJob - beginning batch for #{domains.count} Domains"
       domains.each do |domain|
         WalletModerator::UptimeCheckDebiter.new(domain).debit_for_uptime_checks!
-        WalletModerator::ReleaseCheckDebiter.new(domain).debit_for_uptime_checks!
+        WalletModerator::ReleaseCheckDebiter.new(domain).debit_for_release_checks!
       end
       Rails.logger.info "RunBulkDebitsJob - completed debiting #{domains.count} Domains in #{Time.current - start} seconds."
     end
