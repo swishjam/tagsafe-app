d3d2693a9b0f241b82cbeb5a7c19460b89b98916
Collin Schneider <collin.thomas.schneider@gmail.com>
2022-06-15 12:32:20 -0400

    fix typo in admin controller

--- app/controllers/admin/tag_identifying_data_domains_controller.rb
+++ app/controllers/admin/tag_identifying_data_domains_controller.rb
@@ -4,7 +4,7 @@ module Admin
       tag_identifying_data = TagIdentifyingData.find_by(uid: params[:tag_identifying_datum_uid])
       params[:tag_identifying_data_domain][:tag_identifying_data_id] = tag_identifying_data.id
       tag_identifying_data_domain = tag_identifying_data.tag_identifying_data_domains.create(tag_identifying_data_domain_params)
-      tag_identfinyg_data_domain.apply_to_tags_without_tag_identifying_data
+      tag_identifying_data_domain.apply_to_tags_without_tag_identifying_data
       redirect_to tag_identifying_datum_path(tag_identifying_data)
     end
 
