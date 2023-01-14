53bf4aa68a4ee7b29c7eeb126ef729a73f7e4046
Zach Zimbler <zachzimbler@gmail.com>
2023-01-14 09:46:41 -0800

    Working on the settings page, performance, and releases

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,7 +1,25 @@
 class SettingsController < LoggedInController
   before_action { render_breadcrumbs({ text: 'Settings' }) }
+  def global_settings
+    render_navigation_items(
+      { url: root_path, text: 'Tags' },
+      { url: all_releases_path, text: 'Releases' },
+      { url: page_performance_path, text: 'Page Performance' },
+      { url: settings_path, text: 'Settings' },
+    )
+  end
+
 
   def team_management
-    render_breadcrumbs(text: 'Team Management')
+    render_breadcrumbs(
+      { text: 'Settings', url: settings_path },
+      { text: 'Team Management' },
+    )
+    render_navigation_items(
+      { url: root_path, text: 'Tags' },
+      { url: all_releases_path, text: 'Releases' },
+      { url: page_performance_path, text: 'Page Performance' },
+      { url: settings_path, text: 'Settings', active: true },
+    )
   end
 end
