26c6bcbb86789ce40cbd30af710b9bd2a8d083b4
collin-tagsafe <collin@tagafe.io>
2023-01-14 11:02:52 -0800

    implements PopperJs tooltips

--- app/assets/javascripts/controllers/tooltip_controller.js
+++ app/assets/javascripts/controllers/tooltip_controller.js
@@ -2,20 +2,41 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   connect() {
-    // this.tooltip = new bootstrap.Tooltip(this.element, { customClass: this.element.getAttribute('data-bs-custom-class') || '' });
-    // this.tooltip = new bootstrap.Tooltip(this.element, { 
-    //   trigger: this.element.getAttribute('data-tooltip-trigger') || 'hover',
-    //   customClass: 'tagsafe-tooltip',
-    //   template: `
-    //     <div class="tooltip" role="tooltip">
-    //       <div class="tooltip-inner"></div>
-    //       <div class="tooltip-arrow"></div>
-    //     </div>
-    //   `
-    // });
+    this.element.addEventListener('mouseover', () => {
+      this.tooltip || this._initializePopperTooltip();
+      this.tooltip.update();
+      this.tooltipEl.classList.remove('hidden')
+    });
+    this.element.addEventListener('mouseout', () => this.tooltipEl.classList.add('hidden'));
   }
 
   disconnect() {
     // this.tooltip.dispose();
+  }
+
+  _initializePopperTooltip() {
+    this.tooltipEl = this._createTooltipEl();
+    this.tooltip = Popper.createPopper(this.element, this.tooltipEl, {
+      placement: 'top',
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 8],
+          },
+        },
+      ],
+    });
+  }
+
+  _createTooltipEl() {
+    const el = document.createElement('div');
+    el.classList.add(
+      'tagsafe-tooltip', 'hidden', 'cursor-default', 'max-w-xs', 'border', 'text-sm', 'bg-white', 'text-gray-700', 'px-2', 
+      'py-2', 'rounded-lg', 'shadow-lg'
+    );
+    el.innerText = this.element.getAttribute('title');
+    document.body.appendChild(el);
+    return el;
   }
 }
