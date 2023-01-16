abf8aee37610007beb4ff5b2adb8484ffa6d1057
collin-tagsafe <collin@tagafe.io>
2023-01-15 23:28:36 -0800

    completes login/registration pages updates

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,32 +1,23 @@
 class RegistrationsController < LoggedOutController
   skip_before_action :verify_authenticity_token
+  
   def new
     redirect_to root_path if current_user.present?
-    @hide_logged_out_nav = true
-    @hide_footer = true
-    @user = User.new
-    if params[:container]
-      @container = Container.find_by(uid: params[:container])
-    end
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      if current_container
-        current_container.add_user(@user)
-        current_container.mark_as_registered!
-        Role.USER_ADMIN.apply_to_container_user(@user.container_user_for(current_container))
-        set_current_user(@user)
-        redirect_to root_path
-      else
-        set_current_user(@user)
-        redirect_to new_registration_path
-      end
+    user = User.new(user_params)
+    if user.save
+      set_current_user(user)
+      url_to_go_to = session[:redirect_url] || root_path
+      session.delete(:redirect_url)
+      redirect_to url_to_go_to
     else
-      display_inline_errors(@user.errors.full_messages)
-      @hide_logged_out_nav = true
-      render :new, status: :unprocessable_entity
+      render turbo_stream: turbo_stream.replace(
+        'registration_form',
+        partial: 'registrations/form',
+        locals: { user: user }
+      )
     end
   end
 
