dfd1ee654f87450369d783d2d39fc3f72cd276ec
collin-tagsafe <collin@tagafe.io>
2023-01-22 12:42:45 -0800

    fixes audit notification URL, removes legacy turbo stream

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -154,7 +154,7 @@ def self.run(tag:, tag_version:, page_url:, execution_reason:, initiated_by_cont
         container_user.user.broadcast_notification(
           title: "Audit completed",
           message: "Audit completed for #{tag.tag_snippet.name} with a Tagsafe Score of #{tagsafe_score}.",
-          cta_url: "/containers/#{container.uid}/tags/#{tag.uid}/audits/#{uid}",
+          cta_url: "/containers/#{container.uid}/tag_snippets/#{tag.tag_snippet.uid}/tags/#{tag.uid}/audits/#{uid}",
           cta_text: "View audit",
           image: tag.try_image_url,
         )
@@ -163,7 +163,7 @@ def self.run(tag:, tag_version:, page_url:, execution_reason:, initiated_by_cont
       initiated_by_container_user.user.broadcast_notification(
           title: "Audit completed",
           message: "Audit completed for #{tag.tag_snippet.name} with a Tagsafe Score of #{tagsafe_score}.",
-          cta_url: "/containers/#{container.uid}/tags/#{tag.uid}/audits/#{uid}",
+          cta_url: "/containers/#{container.uid}/tag_snippets/#{tag.tag_snippet.uid}/tags/#{tag.uid}/audits/#{uid}",
           cta_text: "View audit",
           image: tag.try_image_url,
       )
