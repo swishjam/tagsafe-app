cf80c02c0adec3f1beda20859e6347611c68e0e3
collin-tagsafe <collin@tagafe.io>
2023-01-21 16:03:43 -0800

    change param name

--- app/services/tag_manager/find_tags_in_tag_snippet.rb
+++ app/services/tag_manager/find_tags_in_tag_snippet.rb
@@ -1,10 +1,10 @@
 module TagManager
   class FindTagsInTagSnippet
-    def self.find!(encoded_executable_javascript)
+    def self.find!(encoded_tag_snippet_content)
       TagsafeAws::Lambda.invoke_function(
         function_name: "find-tags-#{ENV['LAMBDA_ENVIRONMENT'] || Rails.env}-find-tags",
         async: false,
-        payload: { encoded_tag_snippet_content: encoded_executable_javascript },
+        payload: { encoded_tag_snippet_content: encoded_tag_snippet_content },
       )
     end
   end
