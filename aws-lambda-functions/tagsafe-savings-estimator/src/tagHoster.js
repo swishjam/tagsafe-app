5387dbcee2e5574aacd7b15732b91bb256f378c9
collin-tagsafe <collin@tagafe.io>
2023-02-06 09:08:28 -0800

    makes it an upgrade to host with tagsafe and enable performance monitoring

--- aws-lambda-functions/tagsafe-savings-estimator/src/tagHoster.js
+++ aws-lambda-functions/tagsafe-savings-estimator/src/tagHoster.js
@@ -5,51 +5,60 @@ const AWS = require('aws-sdk');
 require('dotenv').config();
 
 module.exports = class TagHoster {
-  constructor({ pageUrl, firstPartyHosts }) {
+  constructor({ pageUrl, firstPartyHosts, hostAllResources = false }) {
     this.pageUrl = pageUrl;
     this.firstPartyHosts = firstPartyHosts;
+    this.hostAllResources = hostAllResources;
+    this.ignoreQueryParamsWhenOverridingRequests = true;
+
     this.s3Client = new AWS.S3();
     this.s3Directory = `tagsafe-savings-test/${this.pageUrl.replace(/[^a-zA-Z0-9]/g, '_')}`;
-    this.bytesSaved = 0;
-    this.totalOgByteSize = 0;
-    this.totalMinifiedByteSize = 0;
+    // this.bytesSaved = 0;
+    // this.totalOgByteSize = 0;
+    // this.totalMinifiedByteSize = 0;
     this.uploadedS3Keys = [];
   }
 
   async findAllThirdPartyTagsAndUploadThemToS3() {
-    console.log(`Finding all third-party tags on ${this.pageUrl}...`);
+    console.log(`Finding all resources to host on ${this.pageUrl}...`);
     console.log(`Navigating to ${this.pageUrl}...`);
     const puppeteerModerator = new PuppeteerModerator();
     const page = await puppeteerModerator.launch();
     
-    let thirdPartyTagUrls = [];
+    let resourcesToHost = [];
     await page.setRequestInterception(true);
     page.on('request', async req => {
-      if (req.resourceType() === 'script' && !this.firstPartyHosts.includes(new URL(req.url()).host)) {
-        thirdPartyTagUrls.push(req.url());
+      if(this.hostAllResources) {
+        if(req.resourceType() !== 'document') {
+          resourcesToHost.push({ url: req.url(), resourceType: req.resourceType() });
+        }
+      } else if (req.resourceType() === 'script' && !this.firstPartyHosts.includes(new URL(req.url()).host)) {
+        resourcesToHost.push({ url: req.url(), resourceType: req.resourceType() });
       }
       await req.continue();
     })
 
     await page.goto(this.pageUrl, { waituntil: ['domcontentloaded', 'networkidle0'] });
-    // sleep for 5 seconds
     await new Promise(resolve => setTimeout(resolve, 5000));
     await puppeteerModerator.shutdown();
 
-    console.log(`Uploading ${thirdPartyTagUrls.length} third-party tags to S3...`);
-    let tagUrlsToTagsafeCDNMap = {};
-    for(let i in thirdPartyTagUrls) {
-      const tagUrl = thirdPartyTagUrls[i];
-      const cdnUrl = await this._uploadThirdPartyTagsToS3(tagUrl);
+    console.log(`Uploading ${resourcesToHost.length} resources to S3...`);
+    let resourceUrlsToTagsafeCDNMap = {};
+    for (let i in resourcesToHost) {
+      const resource = resourcesToHost[i];
+      const cdnUrl = await this._uploadResourceToS3(resource.url, resource.resourceType);
       if (cdnUrl) {
-        tagUrlsToTagsafeCDNMap[tagUrl] = cdnUrl;
+        const parsedUrl = new URL(resource.url);
+        const urlWithoutQueryParams = `${parsedUrl.protocol}//${parsedUrl.host}${parsedUrl.pathname}`;
+        resourceUrlsToTagsafeCDNMap[resource.url] = cdnUrl;
+        resourceUrlsToTagsafeCDNMap[urlWithoutQueryParams] = cdnUrl;
       }
     }
     console.log(`FOUND ALL THIRD PARTY TAGS AND UPLOADED THEM TO S3! TOTAL BYTES SAVED: ${this.totalOgByteSize - this.totalMinifiedByteSize} (${(this.totalOgByteSize - this.totalMinifiedByteSize) / this.totalOgByteSize * 100}%)`);
     return { 
-      tagUrlsToTagsafeCDNMap,
-      totalOriginalByteSize: this.totalOgByteSize,
-      totalMinifiedByteSize: this.totalMinifiedByteSize,
+      resourceUrlsToTagsafeCDNMap,
+      // totalOriginalByteSize: this.totalOgByteSize,
+      // totalMinifiedByteSize: this.totalMinifiedByteSize,
     };
   }
 
@@ -63,32 +72,47 @@ module.exports = class TagHoster {
     console.log('Purge complete!');
   }
 
-  async _uploadThirdPartyTagsToS3(tagUrl) {
+  async _uploadResourceToS3(resourceUrl, resourceType, attempts = 0) {
     try {
-      console.log(`Uploading third-party tag to S3 ${tagUrl}...`);
+      console.log(`Uploading resource to S3 ${resourceUrl}...`);
       const bucketParams = { Bucket: process.env.S3_BUCKET_NAME, ACL: 'public-read' };
   
-      const tagResponse = await fetch(tagUrl);
-      const tagContents = await tagResponse.text();
-      const ogByteSize = Buffer.byteLength(tagContents, 'utf8');
-      const minifiedTagContents = UglifyJS.minify(tagContents).code;
-      const minifiedByteSize = Buffer.byteLength(minifiedTagContents, 'utf8');
-
-      this.totalOgByteSize += ogByteSize;
-      this.totalMinifiedByteSize += minifiedByteSize;
+      const resourceResponse = await fetch(resourceUrl, { headers: { 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' }});
+      const resourceContents = await resourceResponse.text();
+      // if(resourceType === 'script') {
+      //   const ogByteSize = Buffer.byteLength(resourceContents, 'utf8');
+      //   const minifiedTagContents = UglifyJS.minify(resourceContents).code;
+      //   const minifiedByteSize = Buffer.byteLength(minifiedTagContents, 'utf8');
   
-      const tagKey = tagUrl.replace(/[^a-zA-Z0-9]/g, '_');
+      //   this.totalOgByteSize += ogByteSize;
+      //   this.totalMinifiedByteSize += minifiedByteSize;
+      // }
+  
+      const resourceKey = resourceUrl.replace(/[^a-zA-Z0-9]/g, '_');
+      const contentType = {
+        'script': 'application/javascript',
+        'image': 'image/png',
+        'stylesheet': 'text/css',
+        'font': 'font/woff2',
+      }[resourceType];
       const s3Params = { 
         ...bucketParams, 
-        Key: `${this.s3Directory}/${tagKey}.js`, 
-        Body: minifiedTagContents || tagContents, 
-        ContentType: 'application/javascript' 
+        Key: `${this.s3Directory}/${resourceKey}.js`, 
+        Body: resourceContents, 
+        ContentType: contentType
       };
       await this.s3Client.upload(s3Params).promise();
       this.uploadedS3Keys.push(s3Params.Key);
+      console.log(`Uploaded ${resourceUrl} resource to S3!`);
       return `https://${process.env.CDN_HOST}/${s3Params.Key}`;
     } catch(err) {
-      console.error(`Cant upload third-party tag to S3 ${tagUrl}: ${err.message}...`)
+      console.error(`Failed to upload resource to S3 ${resourceUrl}: ${err.message}, trying again...`);
+      if (attempts < 3) {
+        return this._uploadResourceToS3(resourceUrl, resourceType, attempts + 1);
+      } else {
+        console.error(`Cant upload resource to S3 ${resourceUrl}: ${err.message}, giving up...`);
+        return null;
+      }
     }
   }
 }
